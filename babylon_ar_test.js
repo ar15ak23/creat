c395e34f4d506b1c1cd6fc18a8ae8a6edf9fb04e
J&M design <86296275+ar15ak23@users.noreply.github.com>
2021-08-11 13:53:34 +0530

    Update babylon_ar_test.js

--- babylon_ar_test.js
+++ babylon_ar_test.js
@@ -25,10 +25,10 @@ window.addEventListener('DOMContentLoaded', function(){
         
         let chair;
       //  BABYLON.SceneLoader.ImportMesh(
-        // https://www.dropbox.com/s/elyh8pk9y4inhsx/d.glb?dl=0
+        // https://www.dropbox.com/s/1xx3jm9hgj4ox5d/ccc.glb?dl=0
         //   "./table.glb",
          //   scene,
-        BABYLON.SceneLoader.ImportMesh("", "https://dl.dropbox.com/s/elyh8pk9y4inhsx/", "d.glb", scene,
+        BABYLON.SceneLoader.ImportMesh("", "https://dl.dropbox.com/s/1xx3jm9hgj4ox5d/", "ccc.glb", scene,
             function (mesh) { 
             
          
